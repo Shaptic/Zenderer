d15d39cca41533805f0d5cc9090431899613c45f
Ruskiy69 <zloi.tatarin@yahoo.com>
2013-07-09 13:39:36 -0700

    Direct text streaming and code doc updates

--- include/Zenderer/GUI/Font.hpp
+++ include/Zenderer/GUI/Font.hpp
@@ -45,7 +45,23 @@ namespace gui
         CFont(const void* const owner = nullptr);
         ~CFont();
         
-         /**
+        /**
+         * Streams data for rendering as a string later.
+         *  This allows for direct rendering of a variety of data
+         *  types (anything supporting `operator<<(std::ostream&)`)
+         * 
+         * @tparam  T       Argument type is implicitly determined
+         * @param   data    Data to write out to the stream
+         *
+         * @see     _fonts-examples.html
+         **/
+        template<typename T> inline
+        CFont& operator<<(const T& data)
+        {
+            m_str << data;
+        }
+        
+        /**
          * Loads a TrueType font from disk.
          *  Here, all of the printable characters in the ASCII table
          *  are loaded as bitmaps, then are turned into render-able
@@ -84,8 +100,30 @@ namespace gui
         /// Returns raw font glyph bitmap data.
         const void* const GetData() const;
         
-        /// @todo   Store line height properly.
-        void Render(obj::CEntity& Ent, const string_t& text);
+        /**
+         * Stores render data in the provided for easy rendering.
+         *  The text to render is optional; if it's not provided the
+         *  internally streamed data will be rendered, and if it is,
+         *  it will be rendered. The parameter takes precedence over
+         *  the stream, so if they both exist, the parameter is used.
+         *
+         * @param   Ent     The entity to store render data in
+         * @param   text    The string to render (optional)
+         *
+         * @return  `true` if `Ent` has the rendered data,
+         *          `false` otherwise, or there was nothing to render.
+         *
+         * @pre     The font has been initialized.
+         * @post    `Ent` contains a renderable string using this font.
+         *
+         * @warning Any existing data in the entity is deleted.
+         *
+         * @todo    Store line height properly.
+         **/
+        bool Render(obj::CEntity& Ent, const string_t text = "");
+        
+        /// Clears the internal string stream.
+        void ClearString();
         
     private:
         bool Destroy();        
@@ -93,7 +131,10 @@ namespace gui
 
         color4f_t m_Color;
         FT_Face m_FontFace;
+        
         std::map<char, glyph_t> mp_glyphData;
+        std::stringstream m_str;
+        
         uint16_t m_size;
     };
     
@@ -107,10 +148,14 @@ namespace gui
 /**
  * @class zen::gfx::CFont
  * @description
- *
+ *  Fonts are unique assets that use the FreeType 2 to load TrueType (`.ttf`)
+ *  fonts and render them in an OpenGL-compatible matter.
+ *  They cannot be drawn directly to the screen, but instead will load
+ *  themselves into `obj::CEntity` instances that can then be treated as
+ *  such. 
  *
  * @example Fonts
- * @subsection Standard Font Loading
+ * @subsection Rendering With a Scene
  *  In this example we will demonstrate the standard procedure for
  *  loading a font and rendering some string to the screen.
  *  First, we assume that a valid context has been created and that
@@ -141,7 +186,8 @@ namespace gui
  *  // Clean up, optional.
  *  Assets.Delete(Font);  
  *  @endcode
- *
+ * 
+ * @subsection Rendering Without a Scene
  *  Since scenes are designed to manage entities independently, the
  *  `CFont::Render()` method cannot simply return a `obj::CEntity` 
  *  instance to the user, since then, when using a scene, it would 
@@ -185,5 +231,52 @@ namespace gui
  *
  *  // Clean up, optional.
  *  Assets.Delete(Font);  
- *  @endcode 
+ *  @endcode
+ *
+ * @subsection Streaming Render Data
+ *
+ *  There is a feature allowing for streaming data directly "into" the font 
+ *  instance and then calling `Render()` without a `string_t` parameter, 
+ *  demonstrated below.
+ *
+ *  @code
+ *  obj::CEntity& Ent = m_Scene.AddEntity();
+ *  gui::CFont* pFont = m_Assets.Create<gui::CFont>();
+ *  pFont->Resize(22);
+ *  pFont->LoadFromFile("data/fonts/default.ttf");
+ *
+ *  uint16_t score = 100;
+ *  real_t health = 45.8;
+ *
+ *  pFont << "Score: " << score << "\nHealth: " << health;
+ *  pFont->Render(Ent);
+ *
+ *  // `Ent` now contains the following:
+ *  // Score: 100
+ *  // Health: 45.8
+ *
+ *  pFont->Render(Ent, "Random Data\nLines");
+ *
+ *  // `Ent` now contains the following:
+ *  // Random Data
+ *  // Lines
+ *
+ *  pFont << "\nName: Zenderer";
+ *  pFont->Render(Ent);
+ *
+ *  // `Ent` now contains the following:
+ *  // Score: 100
+ *  // Health: 45.8
+ *  // Name: Zenderer
+ *
+ *  pFont->ClearString();
+ *  pFont->Render(Ent);
+ *
+ *  // `Ent` now contains nothing, and the `Render()` call will
+ *  // return `false` indicating so.
+ *  @endcode
+ *
+ *  As you can see, streamed data is preserved until `ClearString()` is called,
+ *  and the 2<sup>nd</sup> parameter of `Render()` takes precedence over any
+ *  internal stream data, as well.
  **/
