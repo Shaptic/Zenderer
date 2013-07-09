1706bbeea074cbad99d686e3186d47826dbe07ff
Ruskiy69 <zloi.tatarin@yahoo.com>
2013-07-09 11:54:36 -0700

    Created shader exportation script

---
+++ export_shaders.py
@@ -0,0 +1,64 @@
+import os
+
+SHADER_FILE = 'include/Zenderer/CoreGraphics/ShaderFiles.hpp'
+SHADER_PATH = 'data/shaders/'
+
+def shader(path):
+    data = open(path, 'r').readlines()
+    return data, len(data)
+
+def main():
+    header = open(SHADER_FILE, 'r')
+    data = header.readlines()
+    header.close()
+    index = 0
+    
+    for i in xrange(len(data)):
+        if data[i].find('// Begin shader data.') != -1:
+            index = i
+            break
+
+    data = data[:index+1] + ['\n']
+
+    for i in os.listdir(SHADER_PATH):
+        fn = i.split('.')
+        const = '_'.join([x.upper() for x in fn])
+        
+        print "Creating shader constant for %s ... " % i,
+        
+        data.append('%s/// Created from %s.\n' % ((' ' * 4), SHADER_PATH + i))
+        data.append('%sconst char* const %s[] = {\n' % (' ' * 4, const))
+        
+        lines, count = shader(SHADER_PATH + i)
+        for x in xrange(len(lines)):
+            line = lines[x]
+            string = ''
+            
+            if line.strip() != '':
+                string = '%s"%s"' % ((' ' * 8), line[:-1])
+                if ''.join(lines[x+1:]).strip() != '': string += ','
+                
+            data.append(string + '\n')
+            
+        data.append('%s};\n\n' % (' ' * 4))
+        print "Done."
+        
+    print "Writing %d bytes to %s ... " % (sum([len(x) for x in data]), SHADER_FILE),
+    
+    header = open(SHADER_FILE, 'w')
+    header.write(''.join(data))
+    
+    # Close the namespaces / preprocessor stuff
+    header.write('''}
+}
+
+#endif // ZENDERER__CORE_GRAPHICS__SHADER_FILES_HPP
+
+/** @} **/
+    ''')
+    header.close()
+        
+    print "Done."
+
+if __name__ == "__main__": main()
+ 
---
+++ include/Zenderer/CoreGraphics/ShaderFiles.hpp
@@ -0,0 +1,39 @@
+/**
+ * @file
+ *  Zenderer/CoreGraphics/ShaderFiles.hpp - Constants for raw shader data, 
+ *  generated from a script to prevent distributing raw shader files in 
+ *  public release binaries.
+ *
+ * @author      George Kudrayvtsev (halcyon)
+ * @version     1.0
+ * @copyright   Apache License v2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License").         \n
+ *  You may not use this file except in compliance with the License.        \n
+ *  You may obtain a copy of the License at:
+ *  http://www.apache.org/licenses/LICENSE-2.0                              \n
+ *  Unless required by applicable law or agreed to in writing, software     \n
+ *  distributed under the License is distributed on an "AS IS" BASIS,       \n
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n
+ *  See the License for the specific language governing permissions and     \n
+ *  limitations under the License.
+ *
+ * @addtogroup CoreGraphics
+ * @{
+ **/
+
+#ifndef ZENDERER__CORE_GRAPHICS__SHADER_FILES_HPP
+#define ZENDERER__CORE_GRAPHICS__SHADER_FILES_HPP
+
+namespace zen
+{
+namespace gfxcore
+{
+    // Begin shader data.
+
+}
+}
+
+#endif // ZENDERER__CORE_GRAPHICS__SHADER_FILES_HPP
+
+/** @} **/
+    
