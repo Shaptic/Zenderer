0c78361167487a67bc7a506721af4f34ab38582b
Ruskiy69 <zloi.tatarin@yahoo.com>
2013-07-28 15:26:31 -0700

    Split into 2 files.

--- include/Zenderer/Graphics/Polygon.hpp
+++ include/Zenderer/Graphics/Polygon.hpp
@@ -31,61 +31,13 @@ namespace gfx
     class ZEN_API CPolygon : public gfxcore::CDrawable
     {
     public:
-        CPolygon(asset::CAssetManager& Assets) :
-            CDrawable(Assets) {}
-        ~CPolygon()
-        {
-            for(auto& i : m_Verts) delete i;
-            m_Verts.clear();
-        }
+        CPolygon(asset::CAssetManager& Assets);
+        ~CPolygon();
 
-        void AddVertex(const math::vector_t& Position)
-        {
-            gfxcore::vertex_t* V = new gfxcore::vertex_t;
-            V->position = Position;
-            m_Verts.push_back(V);
-        }
+        void AddVertex(const math::vector_t& Position);
+        gfxcore::CDrawable& Create();
 
-        gfxcore::CDrawable& Create()
-        {
-            if(m_Verts.size() <= 2) return (*this);
-
-            uint16_t tris = (m_Verts.size() - 2) * 3;
-            gfxcore::index_t* indices = new gfxcore::index_t[tris];
-
-            for(uint16_t i = 0; i < tris; i += 3)
-            {
-                uint16_t x = i / 3;
-                indices[i] = 0;
-                indices[i+1] = x + 1;
-                indices[i+2] = x + 2;
-            }
-            //indices[tris - 1] = 0;
-
-            m_DrawData.Vertices = new gfxcore::vertex_t[m_Verts.size()];
-            m_DrawData.vcount   = m_Verts.size();
-            m_DrawData.Indices  = indices;
-            m_DrawData.icount   = tris;
-
-            math::vector_t First(m_Verts[0]->position);
-            for(size_t i = 0; i < m_Verts.size(); ++i)
-            {
-                m_DrawData.Vertices[i].position = m_Verts[i]->position;
-                m_DrawData.Vertices[i].color    = m_Verts[i]->color;
-                m_DrawData.Vertices[i].tc       = m_Verts[i]->tc;
-            }
-
-            for(auto& i : m_Verts) delete i;
-            m_Verts.clear();
-
-            return (*this);
-        }
-
-        void SetColor(const color4f_t& Color)
-        {
-            auto i = m_Verts.begin(), j = m_Verts.end();
-            for( ; i != j; ++i) (*i)->color = Color;
-        }
+        void SetColor(const color4f_t& Color);
 
     private:
         std::vector<gfxcore::vertex_t*> m_Verts;
---
+++ src/Graphics/Polygon.cpp
@@ -0,0 +1,63 @@
+#include "Zenderer/Graphics/Polygon.hpp"
+
+using namespace zen;
+using gfx::CPolygon;
+
+CPolygon::CPolygon(asset::CAssetManager& Assets) :
+    CDrawable(Assets)
+{
+    m_Verts.clear();
+}
+
+CPolygon::~CPolygon()
+{
+    for(auto& i : m_Verts) delete i;
+    m_Verts.clear();
+}
+
+void CPolygon::AddVertex(const math::vector_t& Position)
+{
+    gfxcore::vertex_t* V = new gfxcore::vertex_t;
+    V->position = Position;
+    m_Verts.push_back(V);
+}
+
+gfxcore::CDrawable& CPolygon::Create()
+{
+    if(m_Verts.size() <= 2) return (*this);
+
+    uint16_t tris = (m_Verts.size() - 2) * 3;
+    gfxcore::index_t* indices = new gfxcore::index_t[tris];
+
+    for(uint16_t i = 0; i < tris; i += 3)
+    {
+        uint16_t x = i / 3;
+        indices[i] = 0;
+        indices[i+1] = x + 1;
+        indices[i+2] = x + 2;
+    }
+
+    m_DrawData.Vertices = new gfxcore::vertex_t[m_Verts.size()];
+    m_DrawData.vcount   = m_Verts.size();
+    m_DrawData.Indices  = indices;
+    m_DrawData.icount   = tris;
+
+    math::vector_t First(m_Verts[0]->position);
+    for(size_t i = 0; i < m_Verts.size(); ++i)
+    {
+        m_DrawData.Vertices[i].position = m_Verts[i]->position;
+        m_DrawData.Vertices[i].color    = m_Verts[i]->color;
+        m_DrawData.Vertices[i].tc       = m_Verts[i]->tc;
+    }
+
+    for(auto& i : m_Verts) delete i;
+    m_Verts.clear();
+
+    return (*this);
+}
+
+void CPolygon::SetColor(const color4f_t& Color)
+{
+    auto i = m_Verts.begin(), j = m_Verts.end();
+    for( ; i != j; ++i) (*i)->color = Color;
+}
