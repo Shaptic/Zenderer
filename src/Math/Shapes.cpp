6c45bc6a8002e88f76dc9dc0cf2601d2f836728d
Ruskiy69 <zloi.tatarin@yahoo.com>
2013-09-11 16:20:23 -0700

    #15: Moved from header to implementation file.

--- src/Math/Shapes.cpp
+++ src/Math/Shapes.cpp
@@ -118,3 +118,99 @@ bool math::orientation(const tri_t& Tri)
     return (Tri[1].x - Tri[0].x) * (Tri[2].y - Tri[0].y) -
            (Tri[2].x - Tri[0].x) * (Tri[1].y - Tri[0].y);
 }
+
+bool math::triangle_test(const vector_t& V, const tri_t& T)
+{
+    real_t denom = (T[1].y - T[2].y) * (T[0].x - T[2].x)
+                 + (T[2].x - T[1].x) * (T[0].y - T[2].y);
+
+    // Avoid division by zero.
+    if(compf(denom, 0.0)) return true;
+    denom = 1.0 / denom;
+    
+    real_t alpha = denom * ((T[1].y - T[2].y) * (V.x - T[2].x) +
+                            (T[2].x - T[1].x) * (V.y - T[2].y));
+    if(alpha < 0) return false;
+ 
+    real_t beta  = denom * ((T[2].y - T[0].y) * (V.x - T[2].x) +
+                            (T[0].x - T[2].x) * (V.y - T[2].y));
+
+    return (beta > 0 || alpha + beta >= 1);
+}
+
+std::vector<vector_t> math::triangulate(std::vector<vector_t> Polygon)
+{
+    std::vector<uint16_t> reflex;
+    std::vector<vector_t> triangles;    
+    
+    // Determine entire polygon orientation
+    bool ort = orientation(Polygon);
+    
+    // We know there will be vertex_count - 2 triangles made.
+    triangles.reserve(Polygon.size() - 2);
+ 
+    if(Polygon.size() == 3) return Polygon;
+    while(Polygon.size() >= 3)
+    {
+        reflex.clear();
+        int16_t eartip = -1, index = -1;
+        tri_t tri;
+
+        for(auto& i : Polygon)
+        {
+            ++index;
+            if(eartip >= 0) break;
+            
+            uint16_t p = (index > 0) ? index - 1 : Polygon.size() - 1;
+            uint16_t n = (index < Polygon.size()) ? index + 1 : 0;
+            
+            tri[0] = Polygon[p]; tri[1] = i; tri[2] = Polygon[n];
+            
+            if(orientation(tri) != ort)
+            {
+                reflex.emplace_back(index);
+                continue;
+            }
+            
+            bool ear = true;
+            for(auto& j : reflex)
+            {
+                if(j == p || j == n) continue;
+                if(triangle_test(Polygon[j], tri))
+                {
+                    ear = false;
+                    break;
+                }
+            }
+            
+            if(ear)
+            {
+                auto j = Polygon.begin() + index + 1,
+                     k = Polygon.end();
+                 
+                for( ; j != k; ++j)
+                {
+                    auto& v = *j;
+                    if(&v == &Polygon[p] || &v == &Polygon[n]) continue;
+                    if(in_triangle(v, tri))
+                    {
+                        ear = false;
+                        break;
+                    }
+                }
+            }
+            
+            if(ear) eartip = index;
+        }
+        
+        if(eartip < 0) break;
+        
+        // Create the triangulated piece.
+        for(const auto& i : tri) triangles.push_back(i);
+        
+        // Clip the ear from the polygon.
+        Polygon.erase(std::find(Polygon.begin(), Polygon.end(), tri[1]));
+    }
+    
+    return triangles;
+}
