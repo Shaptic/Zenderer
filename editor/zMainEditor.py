c873ef0ba03b2d837f1592e5ef61632247cc3e87
Ruskiy69 <zloi.tatarin@yahoo.com>
2013-09-17 11:55:05 -0700

    #4: Moved editor exportation into its own file.

--- editor/zMainEditor.py
+++ editor/zMainEditor.py
@@ -10,53 +10,12 @@ import pygame
 from zEntity    import *
 from zLight     import *
 from zGUI       import *
-#from zFile     import Exporter
+from zFile      import Exporter
 
 __version__ = '0.0.1-dev'
 __author__ = 'George Kudrayvtsev'
 
 VALID_IMAGES = ['.png', '.tga']
-
-class Exporter:
-    @staticmethod
-    def ExportEntity(f, e):
-        final  = '<entity>\n'
-        final += '    depth=%d\n' % int(e.details['depth'])
-        final += '    position=%d,%d\n' % (int(e.start[0]), int(e.start[1]))
-
-        if e.end[0] != e.start[0] or e.end[1] != e.start[1]:
-            final += '    vertex=0,0\n'
-            final += '    vertex=%d,0\n'    % (e.end[0] - e.start[0])
-            final += '    vertex=%d,%d\n'   % (e.end[0] - e.start[0],
-                                               e.end[1] - e.start[1])
-            final += '    vertex=0,%d\n\n'  % (e.end[1] - e.start[1])
-
-        final += '    stretch=%s\n' % e.details['stretch']
-        final += '    texture=%s\n' % e.details['texture']
-        final += '    attributes=%s\n' % e.details['attributes']
-        final += '</entity>\n\n'
-        f.write(final)
-        f.flush()
-
-    @staticmethod
-    def ExportLight(f, l):
-        final  = '<light type="%s">\n' % l.details['type'].upper()
-        final += '    color=%s\n' % (make2f(l.details['color']))
-        final += '    position=%d,%d\n' % (int(l.start[0]), int(l.start[1]))
-        final += '    attenuation=%s\n' % (make2f(l.details['attenuation']))
-        final += '    brightness=%0.2f\n' % (float(l.details['brightness']))
-        final += '</light>\n\n'
-        f.write(final)
-        f.flush()
-
-    @staticmethod
-    def ExportPolygon(f, p):
-        final  = '<entity>\n'
-        final += '    vertex='
-        final += '\n    vertex='.join('%d,%d' % (int(v[0]), int(v[1])) for v in p)
-        final += '\n</entity>\n\n'
-        f.write(final)
-        f.flush()
 
 class Main:
     def __init__(self):
