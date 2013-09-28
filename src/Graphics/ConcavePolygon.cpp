4cad51561815f547373afc8f02817d86b93a938f
Ruskiy69 <zloi.tatarin@yahoo.com>
2013-09-28 12:31:27 -0700

    Properly generate indices for concave polygons.

--- src/Graphics/ConcavePolygon.cpp
+++ src/Graphics/ConcavePolygon.cpp
@@ -25,43 +25,45 @@ gfx::zPolygon& zConcavePolygon::Create()
 {
     if(m_Verts.size() <= 2) return (*this);
 
-    std::vector<math::vector_t> tris = math::triangulate(m_Verts);
+    m_Tris = math::triangulate(m_Verts);
     std::vector<math::vector_t> verts;
 
     // We minimize the vertices we offload by only storing those
     // that are unique. This process is O(n^2) but who cares?
     uint16_t index = 0;
-    uint16_t count = (m_Verts.size() - 2) * 3;
+    uint16_t count = m_Tris.size();
 
     if(m_DrawData.Indices != nullptr || m_DrawData.icount > 0)
         delete[] m_DrawData.Indices;
 
     gfxcore::index_t* indices = new gfxcore::index_t[count];
 
-    for(auto& i : tris)
+    for(auto& i : m_Tris)
     {
         int idx = find_index_of(verts.begin(), verts.end(), i);
-        if(idx > 0) indices[index++] = idx;
+        if(idx >= 0)
+        {
+            indices[index++] = idx;
+        }
         else
         {
             verts.push_back(i);
-            indices[index] = index++;
+            indices[index++] = verts.size() - 1;
         }
     }
 
     m_DrawData.Indices  = indices;
-    m_DrawData.icount   = count;
+    m_DrawData.icount   = m_Tris.size();
 
-    m_DrawData.Vertices = new gfxcore::vertex_t[m_Verts.size()];
+    m_DrawData.Vertices = new gfxcore::vertex_t[verts.size()];
     m_DrawData.vcount   = verts.size();
 
-    for(size_t i = 0; i < m_Verts.size(); ++i)
+    for(size_t i = 0; i < verts.size(); ++i)
     {
         m_DrawData.Vertices[i].position = std::move(verts[i]);
         m_DrawData.Vertices[i].color    = m_Color;
     }
 
-    m_Tris = std::move(tris);
     m_Verts.clear();
     return (*this);
 }
