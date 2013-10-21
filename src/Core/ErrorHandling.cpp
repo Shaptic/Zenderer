cdc4265896e29690321f8e67e38c733b48f51500
Ruskiy69 <zloi.tatarin@yahoo.com>
2013-10-21 08:28:26 -0700

    Uses clearer args in MessageBox().

--- src/Core/ErrorHandling.cpp
+++ src/Core/ErrorHandling.cpp
@@ -3,7 +3,7 @@
 void zen::error_fallback(const char* message, const char* title)
 {
 #ifdef _WIN32
-    MessageBoxA(NULL, message, title, NULL);
+    MessageBoxA(nullptr, message, title, 0);
 #else
     std::cerr << "[" << title << "] -- " << message << std::endl;
 #endif // _WIN32
